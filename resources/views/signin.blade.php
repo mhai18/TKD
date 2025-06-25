ee9f2e4eac7ec453fc2773d3fea52d94a5be852e
mhai18 <163799087+mhai18@users.noreply.github.com>
2025-06-25 23:07:17 +0800

    Update signin.blade.php

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Sign In | {{ env('APP_NAME') }}</title>
+    <title>Sign-In | {{ env('APP_NAME') }}</title>
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" />
